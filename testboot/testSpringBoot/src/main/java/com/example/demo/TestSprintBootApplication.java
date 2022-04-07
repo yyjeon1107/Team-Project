a0284b5b1214c313dc1062a1d25e5c68854148fb
saejinpark951210 <saejinpark951210@gmail.com>
2022-04-07 17:58:24 +0900

    Update TestSprintBootApplication.java

--- testboot/testSpringBoot/src/main/java/com/example/demo/TestSprintBootApplication.java
+++ testboot/testSpringBoot/src/main/java/com/example/demo/TestSprintBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestSprintBootApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(TestSprintBootApplication.class, args);
 	}
 
