886a22ad96896b89802058ba3e2f7f8cb843798a
saejinpark951210 <saejinpark951210@gmail.com>
2022-04-08 10:14:45 +0900

    popo

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World");
+		System.out.println("hello dsafdsafdsafd");
 	}
 
 }
