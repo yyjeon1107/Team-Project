5b741c80f259a3a2cd772f075c8660ae698ad4d8
testsbsn <sbsn10@naver.com>
2022-04-13 14:36:07 +0900

    성수

---
+++ 회원가입/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,54 @@
+package com.mysite.sbb.question;
+
+
+import javax.validation.Valid;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mysite.sbb.answer.AnswerForm;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/question")
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @RequestMapping("/list")
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<QuestionDto> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
+        return "question_list";
+    }
+
+    @RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
+        QuestionDto question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+    }
+}
