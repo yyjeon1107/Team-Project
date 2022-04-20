7ff23651a6afcf834a72e7e81527f0d2af16e68a
ycy227 <mimiming7@gmail.com>
2022-04-20 13:38:21 +0900

    .
    .

--- WhatToEatToday/src/main/java/team/project/WhatToEatToday/controller/RestController.java
+++ WhatToEatToday/src/main/java/team/project/WhatToEatToday/controller/RestController.java
@@ -1,18 +1,33 @@
 package team.project.WhatToEatToday.controller;
 
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
+import team.project.WhatToEatToday.domain.EatingHouse;
+import team.project.WhatToEatToday.repository.EatingHouseRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @org.springframework.web.bind.annotation.RestController
+@RequiredArgsConstructor
 public class RestController {
 
+	private final EatingHouseRepository eatingHouseRepository;
+	
     @RequestMapping("/message_reset")
     public void messageReset(HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.setAttribute("message", null);
     }
-
+    
+    @ResponseBody
+    @RequestMapping("/data")
+    public Admin a() {
+    	
+    	return eatingHouseRepository.findId("admin");
+    }
 }
--- WhatToEatToday/src/main/java/team/project/WhatToEatToday/repository/EatingHouseRepository.java
+++ WhatToEatToday/src/main/java/team/project/WhatToEatToday/repository/EatingHouseRepository.java
@@ -1,6 +1,8 @@
 package team.project.WhatToEatToday.repository;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.stereotype.Repository;
 import team.project.WhatToEatToday.domain.EatingHouse;
 
@@ -12,11 +14,17 @@ public class EatingHouseRepository {
     private final EntityManager em;
 
     public void save(EatingHouse eatingHouse){
-        if(eatingHouse.getId() == null) {
-            em.persist(eatingHouse);
-        } else {
-            em.merge(eatingHouse);
-        }
+//        if(eatingHouse.getId() == null) {
+//            em.persist(eatingHouse);
+//        } else {
+//            em.merge(eatingHouse);
+//        }
+    	em.persist(eatingHouse);
     }
+
+	public Admin findId(String id) {
+		// TODO Auto-generated method stub
+		return em.find(Admin.class, id);
+	}
 
 }
