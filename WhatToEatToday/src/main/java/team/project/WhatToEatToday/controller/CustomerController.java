a9982b17739db44832a5ea3315034abb9ee80396
yyjeon1107 <103176209+yyjeon1107@users.noreply.github.com>
2022-04-26 15:34:02 +0900

    수정2

--- WhatToEatToday/src/main/java/team/project/WhatToEatToday/controller/CustomerController.java
+++ WhatToEatToday/src/main/java/team/project/WhatToEatToday/controller/CustomerController.java
@@ -1,7 +1,5 @@
 package team.project.WhatToEatToday.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,22 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import team.project.WhatToEatToday.Service.EatingHouseService;
-import team.project.WhatToEatToday.domain.EatingHouse;
-import team.project.WhatToEatToday.domain.Item;
 
 @Controller
 @RequestMapping("/customer")
 @RequiredArgsConstructor
 public class CustomerController {
 
-	private final EatingHouseService eatingHouseService;
 
 	
 	@GetMapping("/mypage")
     public String all(Model model) {
-    	List<EatingHouse> eatingHouses = eatingHouseService.findAll();
-        model.addAttribute("page", "eatinghouses");
-        model.addAttribute("eatinghouses", eatingHouses);
+        model.addAttribute("page", "myPage");
         return "layout";
         
     }
