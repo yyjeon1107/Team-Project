ed3c1bcb0853d5f60e30923ffdde4b236173d47d
ycy227 <mimiming7@gmail.com>
2022-04-27 15:05:53 +0900

    .
    .

---
+++ WhatToEatToday/src/main/java/team/project/WhatToEatToday/repository/EatingHouseItemRepository.java
@@ -0,0 +1,52 @@
+package team.project.WhatToEatToday.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import team.project.WhatToEatToday.domain.EatingHouse;
+import team.project.WhatToEatToday.domain.Item;
+import team.project.WhatToEatToday.domain.member.Manager;
+
+@Repository
+@RequiredArgsConstructor
+public class EatingHouseItemRepository {
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+    	return em.find(Item.class, id);
+    }
+    
+
+    public List<Item> findAll() {
+        return em.createQuery("SELECT e FROM Item e", Item.class)
+                .getResultList();
+    }
+    
+
+    public Item findId(Manager id) { 
+        return em.createQuery("SELECT e FROM Item e where manager_id = :id", Item.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+
+    
+    public void removeItem(Long id) {
+    	Item item = em.find(Item.class, id);
+    	em.remove(item);
+    }
+
+
+}
